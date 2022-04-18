a28da0f54872359bad285a7a858a76a3319d2ae7
sil211112 <silviaj.barrios55@gmail.com>
2022-04-18 09:09:39 -0500

    se agrega el proyecto

---
+++ anime.js
@@ -0,0 +1,17 @@
+anime({
+    targets: 'div.conteiner',
+    translateX: {
+     value: 250,
+     duration: 800,
+    },
+    rotate: {
+     value: 360,
+     duration: 1800,
+    },
+    scale: {
+     value: 2,
+     duration: 1600,
+     delay: 800,
+    },
+    delay: 250,
+   })
